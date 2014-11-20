661f510c49781fa8bede287bd3a703fb63f61826
Robin Rumeau <robin@studio-http.com>
2014-11-20 21:23:12 +0100

    Améliorations diverses sur la home >Inversion des pictos transporteurs et expéditeurs > Ajout du texte de présentation > Modifications des textes des 3 points d'entrée (+ correction orthographique)

--- application/views/stmd2014/content/accueil.php
+++ application/views/stmd2014/content/accueil.php
@@ -1,115 +1,116 @@
 <header class="header_home">
-  <div class="header_top">
-  <center>
-    <div class="logo"><a href="<?php echo base_url("assets"); ?>"><img src="<?php echo base_url("assets"); ?>/img/logo_home.png" alt=" " /></a>
-    <div class="slogan">
-      <p> Le carnet d’adresses des expéditeurs et des transporteurs<br />
-        de marchandises dangereuses </p>
+    <div class="header_top">
+        <center>
+            <div class="logo"><a href="<?php echo base_url("assets"); ?>"><img src="<?php echo base_url("assets"); ?>/img/logo_home.png" alt=" " /></a>
+                <div class="slogan">
+                    <p> Le carnet d’adresses des expéditeurs et des transporteurs<br />
+                        de marchandises dangereuses </p>
+                </div>
+            </div>
+        </center>
+        <div class="social"> <a href="#" class="gplus"></a> <a href="#" class="twitter"></a> <a href="#" class="facebook"></a> <a href="#" class="linkedin"></a> <a href="#" class="vimeo"></a> <a href="<?php echo base_url("login"); ?>" class="connexion">connexion</a> <a href="#" class="copy"></a> <a href="#" class="favoris"></a> </div>
     </div>
-    </div>
-    </center>
-    <div class="social"> <a href="#" class="gplus"></a> <a href="#" class="twitter"></a> <a href="#" class="facebook"></a> <a href="#" class="linkedin"></a> <a href="#" class="vimeo"></a> <a href="<?php echo base_url("login"); ?>" class="connexion">connexion</a> <a href="#" class="copy"></a> <a href="#" class="favoris"></a> </div>
-  </div>
 </header>
 
-    <div class="clear"></div>
-<div class="main">
-<div class="home">
-<center>
-<h1>Moteur de recherche et annuaire</h1>
-
-<p class="top_description">
-Numéro 1 Français pour le transport de marchandises dangereuses
-</p>
-</center>
-<div class="home_search">
-	<form id="search_form" method="get" action="<?php echo base_url("recherche"); ?>/" >
-		<label for="search_field"></label>
-		<input type="search" name="string" id="string" placeholder="Tapez votre recherche. Ex: éditions"/><button id="search"></button>
-	</form>
-</div>
-<div class="container">
-<div class="container_left">
-<center>
-
-
-<a href="<?php echo base_url("annuaire"); ?>/transporteurs_md/" class="transporteur_md">Transporteurs MD
-<span>Tous les prestataires spécialisés dans le transport de marchandises dangereuses sont ici !</span></a>
-
-</center>
-
-</div>
-<div class="container_middle">
-<center>
-
-<a href="<?php echo base_url("annuaire"); ?>/expediteurs_md/" class="expediteur_md">Expéditeurs MD
-<span>Trouver un expéditeur de marchandises dangereuses ici.</span></a>
-
-</center>
-
-</div>
-<div class="container_right">
-<center>
-
-<a href="<?php echo base_url(); ?>rechercher-un-conseiller-a-la-securite/" class="conseiller_md">Conseiller à la sécurité
-<span>Recherchez un conseiller à la sécurité par région, classe et mode de transport.</span></a>
-
-</center>
-
-</div>
-</div>
 <div class="clear"></div>
-</div>
+<div class="main">
+    <div class="home">
+        <center>
+            <h1>Moteur de recherche et annuaire</h1>
+
+            <p class="top_description">
+                Numéro 1 Français pour le transport de marchandises dangereuses
+            </p>
+        </center>
+        <div class="home_search">
+            <form id="search_form" method="get" action="<?php echo base_url("recherche"); ?>/" >
+                <label for="search_field"></label>
+                <input type="search" name="string" id="string" placeholder="Tapez votre recherche. Ex: éditions"/><button id="search"></button>
+            </form>
+        </div>
+        <div class="container">
+            <div class="container_left">
+                <center>
+
+
+                    <a href="<?php echo base_url("annuaire"); ?>/transporteurs_md/" class="transporteur_md">Vous êtes transporteurs de MD ?
+                        <span>Trouvez le prestataire qu’il vous faut.</span></a>
+
+                </center>
+
+            </div>
+            <div class="container_middle">
+                <center>
+
+                    <a href="<?php echo base_url("annuaire"); ?>/expediteurs_md/" class="expediteur_md">Vous êtes expéditeurs de MD?
+                        <span>Trouvez des prestataires pour vous aider à gérer, stocker et transporter vos marchandises dangereuses.</span></a>
+
+                </center>
+
+            </div>
+            <div class="container_right">
+                <center>
+
+                    <a href="<?php echo base_url(); ?>rechercher-un-conseiller-a-la-securite/" class="conseiller_md">Vous recherchez un conseiller à la sécurité?
+                        <span>Trouvez un conseiller ADR externe proche de chez vous.</span></a>
+
+                </center>
+
+            </div>
+        </div>
+        <div class="clear"></div>
+        <p class="home_info">Depuis 2007, www.solutionstmd.fr référence les principaux prestataires pour aider les entreprises à expédier et transporter des marchandises dangereuses. Nous référençons des entreprises ayant un service à destination des expéditeurs et transpor- teurs de marchandises classées à risque : des fabricants d’emballage, des éditeurs de fiche de données de sécurité, des trans- porteurs, des logisticiens, des formateurs, des emballeurs, etc..</p>
+    </div>
 
 </div>
 <div class="home_carousel">
-<div class="home_carousel_cont">
-<div id="ca-container" class="ca-container">
-<div class="ca-wrapper">
+    <div class="home_carousel_cont">
+        <div id="ca-container" class="ca-container">
+            <div class="ca-wrapper">
 
-				<div class="ca-item ca-item-1">
-						<div class="ca-item-main">
-							<h3>L’ ANCS a déjà 10 ans</h3>
-							<p>
+                <div class="ca-item ca-item-1">
+                    <div class="ca-item-main">
+                        <h3>L’ ANCS a déjà 10 ans</h3>
+                        <p>
                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore utate velit esse molestie cons <strong>...</strong>
-                            </p>
-								
-						</div>
-					</div>
-                    
-                    
-                    <div class="ca-item ca-item-2">
-						<div class="ca-item-main">
-							<h3>Mouvements transfrontièresdes déchets</h3>
-							<p>
+                        </p>
+
+                    </div>
+                </div>
+
+
+                <div class="ca-item ca-item-2">
+                    <div class="ca-item-main">
+                        <h3>Mouvements transfrontièresdes déchets</h3>
+                        <p>
                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore utate velit esse molestie cons <strong>...</strong>
-                            </p>
-								
-						</div>
-					</div>
-                    
-                    <div class="ca-item ca-item-3">
-						<div class="ca-item-main">
-							<h3>Arrêté TMD, refonte du rapport annuel d’actitvité</h3>
-							<p>
+                        </p>
+
+                    </div>
+                </div>
+
+                <div class="ca-item ca-item-3">
+                    <div class="ca-item-main">
+                        <h3>Arrêté TMD, refonte du rapport annuel d’actitvité</h3>
+                        <p>
                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore utate velit esse molestie cons <strong>...</strong>
-                            </p>
-								
-						</div>
-					</div>
-                    
-                    <div class="ca-item ca-item-4">
-						<div class="ca-item-main">
-							<h3>Arrêté TMD, refonte du rapport annuel d’actitvité</h3>
-							<p>
+                        </p>
+
+                    </div>
+                </div>
+
+                <div class="ca-item ca-item-4">
+                    <div class="ca-item-main">
+                        <h3>Arrêté TMD, refonte du rapport annuel d’actitvité</h3>
+                        <p>
                             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore utate velit esse molestie cons <strong>...</strong>
-                            </p>
-								
-						</div>
-					</div>
-                    
-</div>
-</div>
-</div>
+                        </p>
+
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </div>
 <div class="clear"></div>
