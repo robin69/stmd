7ae5037ccdaacfd78e34710db6eb571c6cd88430
www-data <www-data@premium.phpnet.org>
2014-11-27 19:46:10 +0100

    modifications sur Connexion

--- application/config/config.php
+++ application/config/config.php
@@ -250,7 +250,7 @@ $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= TRUE;
 $config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'ci_sessions';
-$config['sess_match_ip']		= FALSE;
+$config['sess_match_ip']		= TRUE;
 $config['sess_match_useragent']	= TRUE;
 $config['sess_time_to_update']	= 300;
 
@@ -266,7 +266,7 @@ $config['sess_time_to_update']	= 300;
 |
 */
 $config['cookie_prefix']	= "stmd_";
-$config['cookie_domain']	= ".solutionstmd.com";
+$config['cookie_domain']	= ".en-production.net";
 $config['cookie_path']		= "/";
 $config['cookie_secure']	= FALSE;
 
