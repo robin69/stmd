47d8df29c3f3675f18b8fa04e97d5bfface26e83
www-data <www-data@premium.phpnet.org>
2014-11-28 06:46:09 +0100

    corrections pour l'envois d'email

--- application/config/email.php
+++ application/config/email.php
@@ -10,7 +10,7 @@
 *
 */
 $config['protocol'] 	= 'smtp';
-$config['smtp_host']	= 'smtpauth.phpnet.org';
+$config['smtp_host']	= 'smtp.phpnet.org';
 $config['smtp_user'] 	= 'suivi@en-production.net';
 $config['smtp_port'] 	= '25';						//8025Port spécifique pour phpnet pour contourner les probs d'envois SMTP bloqués (notamment par orange)
 $config['smtp_pass'] 	= 'puce0975';
