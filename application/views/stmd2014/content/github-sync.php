7e1329a832f1976542e7dab3649405640e670049
Robin Rumeau <robin@studio-http.com>
2014-11-30 17:45:21 +0100

    Ajout du fichier de synchro

---
+++ application/views/stmd2014/content/github-sync.php
@@ -0,0 +1,8 @@
+<?php
+    /******************************
+     *  NE PAS SUPPPRIMER
+     * C'est le fichier qui permet la syncro avec
+     * GIT Hub
+     *****************************/
+?><?php `git pull`;
+
