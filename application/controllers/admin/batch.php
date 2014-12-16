98c49622f8a542b4e8a7025b4d021e4ff532463d
Robin Rumeau <robin@studio-http.com>
2014-12-16 15:50:15 +0100

    taches_crons > Création du controller des futurs batchs

---
+++ application/controllers/admin/batch.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+
+    /**********************************************
+     * Class Batch
+     *
+     * Contient tous les batch du site.
+     *
+     */
+
+    class Batch extends CI_Controller{
+
+
+        //On vérifie
+
+
+
+    }
